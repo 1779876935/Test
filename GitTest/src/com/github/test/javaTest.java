4a2d95c3018ca0733987e81238015717eceade30
SunSx <1779876935@qq.com>
2018-01-03 09:49:10 +0800

    测试火星版本

--- GitTest/src/com/github/test/javaTest.java
+++ GitTest/src/com/github/test/javaTest.java
@@ -4,4 +4,8 @@ public class javaTest {
 	public static void main(String[] args) {
 		System.out.println("hello world!");
 	}
+	private void test() {
+		// TODO Auto-generated method stub
+		System.out.println("测试！");
+	}
 }
